42007346f868d0567fda6365c3832274c404e48b
RMDev97 <ranjeevmenon@hotmail.com>
2017-02-09 12:50:40 +0000

    Boilerplate code for the server.py file - implemented with the Flask Microframework

--- server.py
+++ server.py
@@ -3,11 +3,36 @@ server.py implements the server interface layer between the android client and t
 """
 
 from flask import Flask
+import json
+
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "hello Neural Guide!"
+
+
+def caption_res(success, status, text, confidence, improvement_tips):
+    return {
+        "success": success,
+        "status": status,
+        "data": {
+            "text": text,
+            "confidence": confidence,
+            "improvementTips": improvement_tips
+        }
+    }
+
+
+"""
+Version 1 of the caption generation api - will return a caption for a given processed image
+"""
+@app.route("/v1/caption")
+def caption():
+    return json.dumps(caption_res(True, 0, "a thing of some sort...", 1.0, []))
+
 
 if __name__ == "__main__":
     app.run()
