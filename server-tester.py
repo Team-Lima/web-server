f8825b1a5b2ada41152437aaa5f01df107e4175e
Tudor Avram <a96tudor@gmail.com>
2017-03-02 21:31:59 +0000

    server communication running on localhost

--- server-tester.py
+++ server-tester.py
@@ -1,32 +1,59 @@
 import json
 import urllib.request
+import tensorflow as tf
+import base64 as b64
 
-test = 0
 
-test_json = {
-    "test": test
-}
+def get_b64(filename):
+    """
 
-myurl = "http://localhost:5000/getjson"
+    :param filename:        The name of the image file that we want to read
+    :return:                The image as a b64 string
+    """
 
-print("Initialising request")
+    with tf.gfile.GFile(filename, "r") as f:
+        image = f.read()
+        b64_data = b64.b64encode(image)
 
-req = urllib.request.Request(myurl)
-req.add_header('Content-Type', 'application/json; charset=utf-8')
+    b64_data = b64_data.decode()
+    #print(b64_data)
+    #print(type(b64_data))
+    return b64_data
 
-print("Gettint the JSON ready")
 
-jsondata = json.dumps(test_json)
-jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
-req.add_header('Content-Length', len(jsondataasbytes))
+def post_request(output):
+    myurl = "http://localhost:80/getjson"
 
-print("The JSON data is: ", jsondataasbytes)
+    print("Initialising request")
 
-print("Sending the request")
+    req = urllib.request.Request(myurl)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
 
-response = urllib.request.urlopen(req, jsondataasbytes)
+    print("Getting the JSON ready")
 
-print("The result is: ")
-bytes_data = response.read()
+    jsondata = json.dumps(output)
+    jsondataasbytes = jsondata.encode('utf-8')  # needs to be bytes
+    req.add_header('Content-Length', len(jsondataasbytes))
 
-print(bytes_data.decode("utf-8"))
+    #print("The JSON data is: ", jsondataasbytes)
+
+    print("Sending the request")
+
+    response = urllib.request.urlopen(req, jsondataasbytes)
+
+    print("The result is: ")
+    bytes_data = response.read()
+
+    print(bytes_data.decode("utf-8"))
+
+def main():
+
+    image_path = "testing/images/1.jpg"
+    print("Getting base64")
+    output = dict()
+    output["data"] = b64_data = get_b64(image_path)
+    print("Encoded image")
+    post_request(output)
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from image import ImageProcessor
 
 app = Flask(__name__)
 
@@ -24,11 +25,41 @@ def get_json():
             print("It is a JSON !!!")
             #print("The data is: ", request.json)
             data = request.json
-            print(type(data))
+            if len(data) != 1 or (not "data" in data):
+                print("Wrong JSON format")
+                abort(400)
+            else:
+                print("Starting to run the image processor")
+                result = run_image_processor(
+                    img_b64=data["data"]
+                )
+                print("Successfully ran the image processor")
+                return jsonify(result)
+
         else:
+            print("Not a JSON")
             abort(400)
 
-        return jsonify(test_dict)
+
+def run_image_processor(img_b64):
+    """
+        Function that takes the b64 version of the image, creates a new ImageProcessor object and returns
+        the result of the image processing (i.e. the caption and improvement tips
+    :param img_b64:     The image as a b64 string
+    :return:            The results of the image processing
+    """
+    imageProcessor = ImageProcessor(
+        img_id=0,
+        img=img_b64
+    )
+
+    try:
+        imageProcessor.run()
+    except:
+        print("ERROR while processing the image")
+
+    return imageProcessor.get_result()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=80,
+            debug=True)
